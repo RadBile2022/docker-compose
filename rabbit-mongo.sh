10bf2e24a2ad485fe009bfdc1bc2bfc3699df95e
Radar <102400087+RadBile2022@users.noreply.github.com>
2024-06-13 14:00:13 +0700

    Update rabbit-mongo.sh

--- rabbit-mongo.sh
+++ rabbit-mongo.sh
@@ -1,3 +1,3 @@
 docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
 
-docker run -d --name mongodb -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=password -p 27017:27017 mongodb/mongodb-enterprise-server:latest
+docker run -d --name mongodb -e MONGODB_INITDB_ROOT_USERNAME=root -e MONGODB_INITDB_ROOT_PASSWORD=password -p 27017:27017 mongodb/mongodb-enterprise-server:latest
